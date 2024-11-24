91eb19b991cf64e2b80945f87878867a8c351a0d
FIL708 <filip.zebrowski4013@gmail.com>
2024-11-24 12:47:13 +0100

    add ts

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+
+const { PORT } = process.env;
+console.log("aasd")
+app.get("/", (req: Request, res: Response) => {
+    res.send("express");
+});
+
+app.listen(PORT, () => {
+    console.log(`[server]: Server is running at http://localhost:${PORT}`);
+});
