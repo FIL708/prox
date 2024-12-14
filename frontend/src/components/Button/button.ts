8b44fba93e0a554268c585881a1b83543f68d4f5
FIL708 <filip.zebrowski4013@gmail.com>
2024-12-14 21:12:23 +0100

    continue configuring

--- frontend/src/components/Button/button.ts
+++ frontend/src/components/Button/button.ts
@@ -1,5 +1,13 @@
+import { sum } from '../../utils/sum';
+
 const buttons = document.querySelectorAll('.btn');
 
+const alertResult = () => {
+    const result = sum(2, 2);
+    alert(result);
+};
+
 buttons.forEach((button) => {
-    button.addEventListener('click', () => alert('click'));
+    button.addEventListener('click', () => alertResult());
 });
+console.log('Asdasd');
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,49 @@
-console.log('INITIALIZATION');
+const scripts = import.meta.glob(
+    ['./components/**/*.ts', '!./components/**/*.spec.ts'],
+    {
+        eager: true,
+    }
+);
 
-import.meta.glob(['./components/**/*.ts', '!./components/**/*.spec.ts'], {
-    eager: true,
-});
+const styles = import.meta.glob('./styles/**/*.css', { eager: true });
 
-import.meta.glob('./styles/**/*.css', { eager: true });
+console.log(
+    `
+    🚀✨ %cInitialization Process Started! ✨🚀
+    --------------------------------------
+    %c
+    📅  Timestamp: ${new Date().toLocaleString()}
+    🛠️  Environment: ${process.env.NODE_ENV || 'development'}
+    📦  Ready to load resources...
+    
+    `,
+    'color: #4CAF50; font-size: 16px; font-weight: bold;',
+    'color: #2196F3; font-size: 14px;'
+);
+
+const scriptsList = [];
+const stylesList = [];
+
+for (const path in scripts) {
+    scriptsList.push(...path.split('/').splice(-1));
+}
+
+for (const path in styles) {
+    stylesList.push(...path.split('/').splice(-1));
+}
+
+console.log(
+    `
+    🎬✨ %cInitialization Complete! ✨🎬
+    ----------------------------------------
+    📂 %cLoaded Scripts:
+    ${scriptsList.map((script) => `   📜 ${script}`)}
+    🎨 %cLoaded Styles:
+    ${stylesList.map((style) => `   🎨 ${style}`)}
+    
+    🚀 Application is ready to rock!
+    `,
+    'color: #4CAF50; font-size: 16px; font-weight: bold;',
+    'color: #FF9800; font-size: 14px;',
+    'color: #03A9F4; font-size: 14px;'
+);
